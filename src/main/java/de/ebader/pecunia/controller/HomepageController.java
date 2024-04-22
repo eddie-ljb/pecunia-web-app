a908400b19894c6fab9fd36ef34a2df6f02d8bd8
Etienne Bader <baderetienne@gmail.com>
2024-04-22 23:00:17 +0200

    docker fix v2

--- src/main/java/de/ebader/pecunia/controller/HomepageController.java
+++ src/main/java/de/ebader/pecunia/controller/HomepageController.java
@@ -1,7 +1,6 @@
 package de.ebader.pecunia.controller;
 
 import java.sql.Date;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,9 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import de.ebader.pecunia.HibernateUtil;
 import de.ebader.pecunia.entities.inventar.InventarMinecraft;
-import de.ebader.pecunia.services.DataInsertionService;
 import de.ebader.pecunia.services.InventarService;
 
 @Controller
@@ -25,8 +22,6 @@ public class HomepageController {
 	
 	@Autowired
 	InventarService inventarService;
-	
-	DataInsertionService diService = new DataInsertionService();
 	
 
 	@GetMapping(value = "/")
@@ -80,11 +75,5 @@ public class HomepageController {
 		return new ModelAndView("statistik.html");
 	}
 	
-	@GetMapping(value = "/testDaten")
-	public ModelAndView testDaten(Model model) {
-		diService.schreibeDatenInInventarDB("Pecunia", Date.valueOf(LocalDate.now()), (long) 10000, (long) 500, (long) 8, (long) 5);
-		model.addAttribute("appName", appName);
-		return new ModelAndView("homepage.html");
-	}
 
 }
